a17c5f0a4130960ece2fb2669301007786549be4
leverh <leverh@gmail.com>
2023-08-30 08:05:53 +0000

    Added logout functionality. Added hook for navbar burger

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -5,3 +5,6 @@ axios.defaults.baseURL = 'https://bookhub-rdf-api-9aad7672239c.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
--- src/componenets/NavBar.js
+++ src/componenets/NavBar.js
@@ -3,49 +3,112 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addReviewIcon = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/reviews/create"
+    >
+      <i className="far fa-plus-square"></i>Add review
+    </NavLink>
+  );
+  const loggedInIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/reviews"
+      >
+        <i className="fas fa-book"></i>Reviews
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/profile"
+      >
+        <i className="fas fa-user"></i>My Profile
+      </NavLink>
+      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+        <i className="fas fa-sign-out-alt"></i>Sign out
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        <Avatar className={styles.Avatar} src={currentUser?.profile_image} text="Profile" height={40} />
+      </NavLink>
+    </>
+  );
   const loggedOutIcons = (
     <>
-    <NavLink exact className={`${styles.NavLink} nav-link`} activeClassName={styles.Active} to="/signin">
-              <span><i className="fas fa-sign-in-alt"></i>Sign in</span>
-            </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="fas fa-sign-in-alt"></i>Sign in
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <i className="fas fa-user-plus"></i>Sign up
+      </NavLink>
+    </>
+  );
 
-            <NavLink exact className={`${styles.NavLink} nav-link`} activeClassName={styles.Active} to="/signup">
-              <span><i className="fas fa-user-plus"></i>Sign up</span>
-            </NavLink>
-            </>
-  );       
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar
+      expanded={expanded}
+      className={styles.NavBar}
+      expand="md"
+      fixed="top"
+    >
       <Container>
         <NavLink to="/" className="nav-link">
           <Navbar.Brand>
             <img src={logo} alt="BookHub logo" height="40" />
           </Navbar.Brand>
         </NavLink>
-
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        {currentUser && addReviewIcon}
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto text-left navbar-special">
-            <NavLink exact className={`${styles.NavLink} nav-link`} activeClassName={styles.Active} to="/">
-              <span><i className="fas fa-igloo"></i>Home</span>
+          <Nav className={`ml-auto text-left navbar-special ${styles.Nav}`}>
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fas fa-home"></i>Home
             </NavLink>
 
-            <NavLink exact className={`${styles.NavLink} nav-link`} activeClassName={styles.Active} to="/reviews">
-              <span><i className="fas fa-book"></i>Reviews</span>
-            </NavLink>
-
-            <NavLink exact className={`${styles.NavLink} nav-link`} activeClassName={styles.Active} to="/profile">
-              <span><i className="fas fa-user"></i>My Profile</span>
-            </NavLink>
             {currentUser ? loggedInIcons : loggedOutIcons}
-            
           </Nav>
         </Navbar.Collapse>
       </Container>
