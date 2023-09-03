23e097eaf95255b3037a5906e318adf4d5c69294
leverh <leverh@gmail.com>
2023-09-03 16:01:28 +0000

    Added infinite scroll to comments. Fixed multiple bugs to do with axiosreq instance in the comments component

--- src/pages/comments/CommentEditForm.js
+++ src/pages/comments/CommentEditForm.js
@@ -1,3 +1,73 @@
+// import React, { useState } from "react";
+// import Form from "react-bootstrap/Form";
+// import { axiosReq } from "../../api/axiosDefaults";
+// import styles from "../../styles/CommentCreateEditForm.module.css";
+
+// function CommentEditForm(props) {
+//   const { id, content, setShowEditForm, setComments } = props;
+//   const [formContent, setFormContent] = useState(content);
+
+//   const handleChange = (event) => {
+//     setFormContent(event.target.value);
+//   };
+
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     try {
+//       await axiosReq.put(`/comments/${id}/`, {
+//         content: formContent.trim(),
+//       });
+//       setComments((prevComments) => ({
+//         ...prevComments,
+//         results: prevComments.results.map((comment) => {
+//           return comment.id === id
+//             ? {
+//                 ...comment,
+//                 content: formContent.trim(),
+//                 updated_at: "now",
+//               }
+//             : comment;
+//         }),
+//       }));
+//       setShowEditForm(false);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
+
+//   return (
+//     <Form onSubmit={handleSubmit}>
+//       <Form.Group className="pr-1">
+//         <Form.Control
+//           className={styles.Form}
+//           as="textarea"
+//           value={formContent}
+//           onChange={handleChange}
+//           rows={2}
+//         />
+//       </Form.Group>
+//       <div className="text-right">
+//         <button
+//           className={styles.Button}
+//           onClick={() => setShowEditForm(false)}
+//           type="button"
+//         >
+//           cancel
+//         </button>
+//         <button
+//           className={styles.Button}
+//           disabled={!formContent.trim()}
+//           type="submit"
+//         >
+//           save
+//         </button>
+//       </div>
+//     </Form>
+//   );
+// }
+
+// export default CommentEditForm;
+
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import { axiosReq } from "../../api/axiosDefaults";
@@ -14,9 +84,9 @@ function CommentEditForm(props) {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axiosReq.put(`/comments/${id}/`, {
-        content: formContent.trim(),
-      });
+      const formData = new FormData();
+      formData.append('content', formContent.trim());
+      const { data } = await axiosReq.put(`/comments/${id}/`, formData);
       setComments((prevComments) => ({
         ...prevComments,
         results: prevComments.results.map((comment) => {
@@ -32,8 +102,10 @@ function CommentEditForm(props) {
       setShowEditForm(false);
     } catch (err) {
       console.log(err);
+      console.log(err.response.data);
     }
   };
+  
 
   return (
     <Form onSubmit={handleSubmit}>
