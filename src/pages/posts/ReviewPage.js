529062f24c218433d83ca914e85cae824adeadcf
leverh <leverh@gmail.com>
2023-09-03 12:20:12 +0000

    Set up the basic comment functionality and tested. Added a comment.js page to handle it

--- src/pages/posts/ReviewPage.js
+++ src/pages/posts/ReviewPage.js
@@ -6,54 +6,67 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import Review from "./Review";
+import Comment from "../comments/Comment";
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function ReviewPage() {
-    const { id } = useParams();
-    const [review, setReview] = useState(null);
-    const currentUser = useCurrentUser();
-    const profile_image = currentUser?.profile_image;
-    const [comments, setComments] = useState({ results: [] });
+  const { id } = useParams();
+  const [review, setReview] = useState({ results: [] });
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
-    useEffect(() => {
-        const fetchReview = async () => {
-            try {
-                const { data } = await axiosReq.get(`/posts/${id}`);
-                console.log(data);
-                setReview(data);
-            } catch (err) {
-                console.error(err);
-            }
-        };
-    
-        fetchReview();
-    }, [id]);
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: review }, { data: comments }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`),
+        ]);
+        setReview({ results: [review] });
+        setComments(comments);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-    return (
-        <Row className="h-100">
-            <Col className="py-2 p-0 p-lg-2" lg={8}>
-                <p>Popular profiles for mobile</p>
-                <Review {...review} setReview={setReview} reviewPage />
-                <Container className={appStyles.Content}>
-                    {currentUser ? (
-                        <CommentCreateForm
-                            profile_id={currentUser.profile_id}
-                            profileImage={profile_image}
-                            post={id}
-                            setPost={setReview}
-                            setComments={setComments}
-                        />
-                    ) : comments.results.length ? (
-                        "Comments"
-                    ) : null}
-                </Container>
-            </Col>
-            <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-                Popular profiles for desktop
-            </Col>
-        </Row>
-    );
+    handleMount();
+  }, [id]);
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>Popular profiles for mobile</p>
+        <Review {...review.results[0]} setReview={setReview} reviewPage />
+        <Container className={appStyles.Content}>
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setPost={setReview}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+          {comments.results.length ? (
+            comments.results.map((comment) => (
+              <Comment key={comment.id} {...comment} />
+            ))
+          ) : currentUser ? (
+            <span>No comments yet, but you could be the first one to comment!</span>
+          ) : (
+            <span>No comments yet!</span>
+          )}
+        </Container>
+      </Col>
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        Popular profiles for desktop
+      </Col>
+    </Row>
+  );
 }
 
 export default ReviewPage;
