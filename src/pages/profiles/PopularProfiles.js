ddce3bc0a21c26f8a5f79fefce19a7852db4e7a2
leverh <leverh@gmail.com>
2023-09-17 12:37:55 +0000

    Added styles for the popular profile component for small screen sizes

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -4,21 +4,20 @@ import appStyles from "../../App.module.css";
 import Asset from "../../components/Asset";
 import { useProfileData } from "../../contexts/ProfileDataContext";
 import Profile from "./Profile";
+import styles from '../../styles/PopularProfiles.module.css';
 
 const PopularProfiles = ({ mobile }) => {
   const { popularProfiles } = useProfileData();
 
   return (
     <Container
-      className={`${appStyles.Content} ${
-        mobile && "d-lg-none text-center mb-3"
-      }`}
-    >
+    className={`${appStyles.Content} ${mobile && "d-lg-none text-center mb-3"} ${styles.popularProfilesContainer}`}
+>
       {popularProfiles.results.length ? (
         <>
           <p>Our Users:</p>
           {mobile ? (
-            <div className="d-flex justify-content-around">
+            <div className={`${styles.profileContainer} ${mobile && styles.mobileProfileContainer}`}>
               {popularProfiles.results.slice(0, 4).map((profile) => (
                 <Profile key={profile.id} profile={profile} mobile />
               ))}
