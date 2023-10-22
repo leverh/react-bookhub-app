381c87abd909e9efae6689ec0170ccf777e55d96
leverh <leverh@gmail.com>
2023-10-22 12:44:19 +0000

    Added a return key functionality to the NYT review page

--- src/pages/NYTReviews.js
+++ src/pages/NYTReviews.js
@@ -24,10 +24,10 @@ const NYTReviews = () => {
         fetchTop10Books();  // Fetch top 10 books on component mount
     }, []);
 
-    const getReviews = async (title) => {  
+    const getReviews = async () => {  
         try {
             setLoading(true);
-            const reviewsData = await fetchNYTReviews(title);
+            const reviewsData = await fetchNYTReviews(searchTerm);
 
             setReviews(reviewsData);
             setLoading(false);
@@ -37,19 +37,24 @@ const NYTReviews = () => {
         }
     };
 
+    const handleSearch = (event) => {
+        event.preventDefault(); 
+        getReviews();
+    };
+
     return (
         <div className={styles.container}>
             <h2 className={styles.header}>New York Times Book Reviews</h2>
-            <div className={styles.searchBox}>
-            <input 
-                type="text" 
-                value={searchTerm} 
-                onChange={e => setSearchTerm(e.target.value)} 
-                placeholder="Search for a book..."
-                className={styles.searchInput}
-            />
-            <button onClick={() => getReviews(searchTerm)} className={`${btnStyles.Button} ${styles.searchButton}`}>Search</button>
-            </div>
+            <form onSubmit={handleSearch} className={styles.searchBox}>
+                <input 
+                    type="text" 
+                    value={searchTerm} 
+                    onChange={e => setSearchTerm(e.target.value)} 
+                    placeholder="Search for a book..."
+                    className={styles.searchInput}
+                />
+                <button type="submit" className={`${btnStyles.Button} ${styles.searchButton}`}>Search</button>
+            </form>
             {loading && <p>Loading reviews...</p>}
             {error && <p>Error: {error}</p>}
             
@@ -69,18 +74,18 @@ const NYTReviews = () => {
             {/* Display top 10 books */}
             <h3 className={styles.itemHeader}>Top 10 NYT Best Sellers</h3>
             <ul className={styles.top10List}>
-    {top10.map((book, index) => (
-        <li key={index} className={styles.top10Item}>
-            <img src={book.book_image} alt={book.title} className={styles.bookImage} />
-            <h4 className={styles.top10Header}>{book.title}</h4>
-            <p className={styles.author}>By: {book.author}</p>
-            <p className={styles.description}>Plot: {book.description}</p>
-            <p className={styles.weeksOnList}>Weeks on List: {book.weeks_on_list}</p>
-            <p className={styles.isbn}>ISBN: {book.primary_isbn13}</p>
-        </li>
-    ))}
-</ul>
-<ScrollToTopButton />
+                {top10.map((book, index) => (
+                    <li key={index} className={styles.top10Item}>
+                        <img src={book.book_image} alt={book.title} className={styles.bookImage} />
+                        <h4 className={styles.top10Header}>{book.title}</h4>
+                        <p className={styles.author}>By: {book.author}</p>
+                        <p className={styles.description}>Plot: {book.description}</p>
+                        <p className={styles.weeksOnList}>Weeks on List: {book.weeks_on_list}</p>
+                        <p className={styles.isbn}>ISBN: {book.primary_isbn13}</p>
+                    </li>
+                ))}
+            </ul>
+            <ScrollToTopButton />
         </div>
     );
 }
